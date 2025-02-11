65ef4e00d1e5b0dc9c325c9a408d1608b95dbbc2
Jeffersonalanmm <jefferson.alan@ufape.edu.br>
2025-02-11 17:38:48 -0300

    alteraçoes no compile_all e no rapl de quicksort para teste

--- C/quicksort/RAPL/main.c
+++ C/quicksort/RAPL/main.c
@@ -1,87 +1,49 @@
-
-
 #include <stdio.h>
-#include <time.h>
+#include <sys/time.h>
 #include <math.h>
 #include <string.h>
 #include "rapl.h"
 
-#define RUNTIME 1
+#define NTIMES 1000
 
-
-int main (int argc, char **argv) 
-{ char command[500]="",language[500]="", test[500]="", path[500]="";
-  int  ntimes = 1000;
-  int  core = 0;
-  int  i=0;
-
-#ifdef RUNTIME
-  //clock_t begin, end;
-  double time_spent;
-  struct timeval tvb,tva;
-#endif
-  
-  FILE * fp;
-
-  //Run command
-//  strcpy(command, "./" );
-  strcat(command,argv[1]);
-  //Language name
-  strcpy(path,"../");
-
-
-  strcpy(language,argv[2]);
-  strcat(language,".csv");
-  strcat(path,language);
-  //Test name
-  strcpy(test,argv[3]);
-
-  //ntimes = atoi (argv[2]);
- 
-
-  fp = fopen(path,"a");
-
-  rapl_init(core);
-
-  //fprintf(fp,"Package , CPU , GPU , DRAM? , Time (sec) \n");
-  
-  for (i = 0 ; i < ntimes ; i++)
-    {  
- 
-    	fprintf(fp,"%s ; ",test);
- 	
-	      
-		#ifdef RUNTIME
-		        //begin = clock();
-				gettimeofday(&tvb,0);
-		#endif
-	
-	rapl_before(fp,core);
-	
-        system(command);
-
-	rapl_after(fp,core);
-
-		#ifdef RUNTIME
-			//end = clock();
-			//time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-			gettimeofday(&tva,0);
-			time_spent = (tva.tv_sec-tvb.tv_sec)*1000000 + tva.tv_usec-tvb.tv_usec;
-			time_spent = time_spent / 1000;
-		#endif
-			
-
-		#ifdef RUNTIME	
-			fprintf(fp," %G \n",time_spent);
-		#endif	
+int main(int argc, char **argv) {
+    if (argc < 4) {
+        fprintf(stderr, "Uso: %s <comando> <linguagem> <teste>\n", argv[0]);
+        return 1;
     }
     
-
-  fclose(fp);
-  fflush(stdout);
-
-  return 0;
+    char command[500] = "";
+    char path[500] = "../";
+    strcat(command, argv[1]);
+    strcat(path, argv[2]);
+    strcat(path, ".csv");
+    
+    FILE *fp = fopen(path, "a");
+    if (!fp) {
+        perror("Erro ao abrir arquivo");
+        return 1;
+    }
+    
+    int core = 0;
+    rapl_init(core);
+    
+    for (int i = 0; i < NTIMES; i++) {
+        fprintf(fp, "%s ; ", argv[3]);
+        
+        struct timeval tvb, tva;
+        gettimeofday(&tvb, NULL);
+        
+        rapl_before(fp, core);
+        system(command);
+        rapl_after(fp, core);
+        
+        gettimeofday(&tva, NULL);
+        double time_spent = (tva.tv_sec - tvb.tv_sec) * 1000000 + (tva.tv_usec - tvb.tv_usec);
+        time_spent /= 1000;  // Converte para milissegundos
+        
+        fprintf(fp, " %.3f \n", time_spent);
+    }
+    
+    fclose(fp);
+    return 0;
 }
-
-
-
--- C/quicksort/compile_all.py
+++ C/quicksort/compile_all.py
@@ -1,49 +1,43 @@
-import sys
 import os
+import sys
 from subprocess import Popen, PIPE, call
 from lazyme.string import color_print
 
-path = '.'
-action = 'compile'
-
 def file_exists(file_path):
-    return os.path.isfile(file_path) if file_path else False
+    return os.path.isfile(file_path)
 
 def execute_command(root, action):
     makefile = os.path.join(root, "Makefile")
+    
     if file_exists(makefile):
-        cmd = f'cd {root}; make {action}'
+        cmd = f'cd {root} && make {action}'
         process = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
         std_out, std_err = process.communicate()
         
-        if action in {'compile', 'run'}:
-            if process.returncode != 0:
-                err_msg = f"{std_err.strip().decode()}. Code: {process.returncode}"
-                color_print(f'[E] Error on {root}: ', color='red', bold=True)
-                print(err_msg)
-            elif std_err:
-                color_print('[OK]', color='green')
-            else:
-                color_print('[OK]', color='green')
+        if process.returncode != 0:
+            color_print(f'[E] Error in {root}: {std_err.strip().decode()}', color='red', bold=True)
+        else:
+            color_print(f'[OK] {root} - {action} completed', color='green')
         
         if action == 'measure':
             call(['sleep', '5'])
 
 def main():
-    for root, dirs, files in os.walk(path):
-        print(f'Checking {root}')
-        execute_command(root, action)
+    path = '.'
+    action = 'compile'
+    
+    if len(sys.argv) == 2:
+        action = sys.argv[1]
+        if action not in {'compile', 'run', 'clean', 'measure'}:
+            color_print(f'Error: Unrecognized action "{action}"', color='red')
+            sys.exit(1)
+    
+    color_print(f'Performing "{action}" action...', color='yellow', bold=True)
+    
+    for root, _, files in os.walk(path):
+        if "Makefile" in files:
+            print(f'Processing {root}...')
+            execute_command(root, action)
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2:
-        act = sys.argv[1]
-        if act in {'compile', 'run', 'clean', 'measure'}:
-            color_print(f'Performing "{act}" action...', color='yellow', bold=True)
-            action = act
-        else:
-            color_print(f'Error: Unrecognized action "{act}"', color='red')
-            sys.exit(1)
-    else:
-        color_print('Performing "compile" action...', color='yellow', bold=True)
-
     main()
